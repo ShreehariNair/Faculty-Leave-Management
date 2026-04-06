0ea90b71ad502cca6cc94d55649170d76eac66dd
ShreehariNair <nsachin23comp@student.mes.ac.in>
2026-04-06 06:31:36 +0530

    Add leave balance feature

--- backend/server.js
+++ backend/server.js
@@ -37,4 +37,9 @@ app.use("/api/faculty-timetable", require("./routes/facultyTimetableRoutes"));
 
 app.get("/", (req, res) => res.send("Faculty Leave Management API Running"));
 
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
 module.exports = app;
